eae5c17116acff9c6113f5d16d8ac1bd3793de9b
JoaoLuizGL <joaoluiz.glira@gmail.com>
2025-06-07 14:17:22 -0300

    feat: Mudança em tela ainda não desenvolvida

--- src/routes/Character.tsx
+++ src/routes/Character.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Character: React.FC = () => {
     return (
         <div>
-            <h1>TESTE CHARACTER</h1>
+            <h1>{window.location.pathname}</h1>
         </div>
     );
 };
