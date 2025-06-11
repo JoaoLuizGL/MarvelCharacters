02f55180012b8f83d55f6299bdfdf5bf90a082da
JoaoLuizGL <joaoluiz.glira@gmail.com>
2025-06-10 22:23:36 -0300

    feat: Adicionando tooltips

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -8,14 +8,14 @@ const LoginForm = () => {
             <form>
                 <div className={classes.formGroup}>
                     <label htmlFor="username">Usuário:</label>
-                    <input type="text" id="username" name="username" placeholder="Digite o usuário" required />
+                    <input type="text" id="username" name="username" placeholder="Digite o usuário" title="Informe o nome de usuário" required />
                 </div>
                 <div className={classes.formGroup}>
                     <label htmlFor="password">Senha:</label>
-                    <input type="password" id="password" name="password" placeholder="Digite a senha" required />
+                    <input type="password" id="password" name="password" placeholder="Digite a senha" title="Informe a senha do usuário" required />
                 </div>
                 <Link to="/home" className={classes.link}>
-                    <button type="submit">Entrar</button>
+                    <button type="submit" title="Clique para ver os personagens!">Entrar</button>
                 </Link>
             </form>
         </div>
